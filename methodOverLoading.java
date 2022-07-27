d4fb64d18e02f297d2849c0ddf5aadb60fb38d6f
Raish Mirza <f.mirza.raish99@gmail.com>
2022-07-27 20:32:27 +0530

    Create methodOverLoading.java

---
+++ methodOverLoading.java
@@ -0,0 +1,27 @@
+public class SmO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sum(10,15);
+		sum(50,60,70);
+		sum(1,2,3,4);
+		
+	
+
+	}
+	public static int sum/*sumOfTwo*/ (int a , int b) {
+		System.out.println(a+b);
+		return a+b;
+	}
+	
+	public static int sum/*sumOfThree*/ (int a ,int b , int c) {
+		System.out.println(a+b+c);
+		return a+b+c;
+	}
+	public static int sum/*sumOfFour*/ (int a, int b, int c, int d) {
+		System.out.println(a+b+c+d);
+		return a+b+c+d;
+	}
+
+}
